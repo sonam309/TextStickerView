183f83de1a547d5d5b085aa5bc17e182b5c75a9a
user <sona.mobisoft@gmail.com>
2021-02-23 16:04:15 +0530

    bug fixes

--- app/src/main/java/com/textsticker/MainActivity.java
+++ app/src/main/java/com/textsticker/MainActivity.java
@@ -3,12 +3,26 @@ package com.textsticker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.textstickerlib.StickerTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout parent_fl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        parent_fl=findViewById(R.id.parent_fl);
+
+
+        // add a stickerText to canvas
+        StickerTextView tv_sticker = new StickerTextView(this);
+        tv_sticker.setText("hello everyOne");
+        parent_fl.addView(tv_sticker);
+
     }
 }
